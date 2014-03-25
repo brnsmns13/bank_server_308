ef7922dfc7e71b74352e2c722c592f8bdc2ce791
brnsmns13 <brnsmns13@gmail.com>
2014-03-24 20:55:37 -0500

    Succesfully create root request

--- bank_server.c
+++ bank_server.c
@@ -2,14 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 #include "Bank.c"
+#include "bank_server.h"
+
+node *root;
+node *last;
+pthread_mutex_t list_mutex;
+int run = 1;
 
 int main(int argc, char const *argv[])
 {
-    int num_worker_threads, num_accounts;
+    int num_worker_threads, num_accounts, request_number;
     char *output_file_name;
     FILE *output_file;
+    pthread_t threads[num_worker_threads];
+    account *accounts;
 
     // Check correct args
     if (argc != 4)
@@ -21,33 +30,72 @@ int main(int argc, char const *argv[])
     // Get args from command line
     num_worker_threads = atoi(argv[1]);
     num_accounts = atoi(argv[2]);
+    output_file_name = (char *) malloc(sizeof(char *) * 128);
     strcpy(output_file_name, argv[3]);
-    output_file = fopen(output_file_name, "w");
+    output_file = fopen(output_file_name, "w+");
 
     // Init bank accounts
     if(!initialize_accounts(num_accounts))
     {
-        printf("Error creating accounts.\n");
+        printf("- Error creating accounts.\n");
+        return 0;
     }
     else 
     {
         printf("* Created %d accounts\n", num_accounts);
     }
 
+    // Create mutex for each account
+    int i;
+    accounts = (int *) malloc(sizeof(account) * num_accounts);
+    for(i = 1; i <= num_accounts; i++)
+    {
+        account acct;
+        pthread_mutex_init(&acct.lock, NULL);
+        acct.account_id = i;
+        accounts[i-1] = acct;
+    }
     // Init worker threads
-    for(int i = 0; i < num_worker_threads; i++)
+    for(i = 0; i < num_worker_threads; i++)
     {
         // Create new thread
+        pthread_create(&threads[i], 0, worker_routine, NULL);
     }
 
     // Enter main thread loop
-    while(1)
+    request_number = 1;
+    root = (node *) malloc(sizeof(node));
+    last = (node *) malloc(sizeof(node));
+    pthread_mutex_init(&list_mutex, NULL);
+    while(run)
     {
         // Get user input
+        char *command;
+        command = get_user_command();
 
-        // Add task to worker queue
+        // Check for exit
+        if(!strcmp("EXIT", command))
+        {
+            run = 0;
+            break;
+        }
 
+        // Create a new request
+        request req;
+        gettimeofday(&req.time_start, NULL);
+        strcpy(req.command, command);
+        req.request_id = request_number++;
+
+        // Push request to queue
+        pthread_mutex_lock(&list_mutex);
+        root->next_node = NULL;
+        root->req = req;
+        pthread_mutex_unlock(&list_mutex);
+
+        printf("Command: %s\n", root->req.command);
     }
+
+    // Do cleanup
 
 
     return 0;
@@ -56,8 +104,24 @@ int main(int argc, char const *argv[])
 // routine for worker threads to run
 void worker_routine()
 {
-    // Wait for tasks to be pushed to queuq
+    // Wait for tasks to be pushed to queue
 
     // Pick up task
+
+}
+
+// Take a single request
+void process_request(request req)
+{
+
+}
+
+// Read line from STDIN and return
+char* get_user_command()
+{
+    char user_in[INPUT_LENGTH];
+    fgets(user_in, INPUT_LENGTH, stdin);
+    user_in[strlen(user_in)-1] = 0;
+    return user_in;
 
 }
